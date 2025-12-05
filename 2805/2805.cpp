6e56cfe9659906dbbc560690a28e1115edf6a03b
wer9808 <wer9808@naver.com>
2025-12-05 11:56:57 +0900

    chore: integrate

---
+++ 2805/2805.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    int N, M;
+
+    scanf("%d %d", &N, &M);
+
+    return 0;
+}
---
+++ 9095/9095.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
